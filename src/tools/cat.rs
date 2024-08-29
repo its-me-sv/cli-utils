1f52b7048dc8060e3ae7d8d279b9561e6792bbc5
Suraj Vijay <surajcpp115@gmail.com>
2024-08-29 20:25:25 +0530

    added seperate handler for CatParse to parse line

--- src/tools/cat.rs
+++ src/tools/cat.rs
@@ -45,60 +45,60 @@ impl CatParser {
     fn parse_file_contents(&self, file_content: String, line_count: &mut u128) -> String {
         let total_lines = file_content.lines().count();
         let digits_len = total_lines.to_string().len();
-        let args = &self.args;
 
         file_content.lines().fold(
             String::with_capacity(file_content.len() + total_lines * 10),
             |mut result, line| {
-                let mut line = line.to_string();
-
-                if args.show_all_characters
-                    || args.show_non_printing_characters_and_end_with_dollar
-                    || args.show_non_printing_characters_except_tabs_and_eol
-                {
-                    let mut new_string = String::with_capacity(line.len() * 2);
-                    line.chars().for_each(|c| match c {
-                        '\0' => new_string.push_str("^@"),
-                        '\x01'..='\x1F' => {
-                            new_string.push_str(&format!("^{}", (c as u8 + 64) as char))
-                        }
-                        '\x7F' => new_string.push_str("^?"),
-                        _ => new_string.push(c),
-                    });
-                    line = new_string;
-                }
-
-                if (args.show_tab_as_i || args.show_all_characters)
-                    && !args.show_non_printing_characters_and_end_with_dollar
-                    && !args.show_non_printing_characters_except_tabs_and_eol
-                {
-                    line = line.replace("\t", "^I");
-                }
-
-                if args.numbered_list_excluding_non_blank_lines && !line.is_empty() {
-                    line = format!("{:width$}  {line}", line_count, width = digits_len + 2);
-                    *line_count += 1;
-                }
-
-                if args.numbered_list_including_blank_lines
-                    && !args.numbered_list_excluding_non_blank_lines
-                {
-                    line = format!("{:width$}  {line}", line_count, width = digits_len + 2);
-                    *line_count += 1;
-                }
-
-                if (args.display_dollar_at_line_end
-                    || args.show_all_characters
-                    || args.show_non_printing_characters_and_end_with_dollar)
-                    && !args.show_non_printing_characters_except_tabs_and_eol
-                {
-                    line.push('$');
-                }
-
-                result.push_str(&line);
+                result.push_str(&self.parse_line(line, line_count, digits_len));
                 result.push('\n');
                 result
             },
         )
+    }
+
+    fn parse_line(&self, line: &str, line_count: &mut u128, digits_len: usize) -> String {
+        let args = &self.args;
+        let mut line = line.to_string();
+
+        if args.show_all_characters
+            || args.show_non_printing_characters_and_end_with_dollar
+            || args.show_non_printing_characters_except_tabs_and_eol
+        {
+            let mut new_string = String::with_capacity(line.len() * 2);
+            line.chars().for_each(|c| match c {
+                '\0' => new_string.push_str("^@"),
+                '\x01'..='\x1F' => new_string.push_str(&format!("^{}", (c as u8 + 64) as char)),
+                '\x7F' => new_string.push_str("^?"),
+                _ => new_string.push(c),
+            });
+            line = new_string;
+        }
+
+        if (args.show_tab_as_i || args.show_all_characters)
+            && !args.show_non_printing_characters_and_end_with_dollar
+            && !args.show_non_printing_characters_except_tabs_and_eol
+        {
+            line = line.replace("\t", "^I");
+        }
+
+        if args.numbered_list_excluding_non_blank_lines && !line.is_empty() {
+            line = format!("{:width$}  {line}", line_count, width = digits_len + 2);
+            *line_count += 1;
+        }
+
+        if args.numbered_list_including_blank_lines && !args.numbered_list_excluding_non_blank_lines
+        {
+            line = format!("{:width$}  {line}", line_count, width = digits_len + 2);
+            *line_count += 1;
+        }
+
+        if (args.display_dollar_at_line_end
+            || args.show_all_characters
+            || args.show_non_printing_characters_and_end_with_dollar)
+            && !args.show_non_printing_characters_except_tabs_and_eol
+        {
+            line.push('$');
+        }
+        line
     }
 }
